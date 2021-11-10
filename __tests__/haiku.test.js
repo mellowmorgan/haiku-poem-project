57766d7077f420c1b715d42d20c813ba48b34a75
mellowmorgan <morgan.waites@gmail.com>
2021-11-10 10:24:32 -0800

    Add test for checkLines method

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,12 +2,20 @@ import { Haiku } from './../src/haiku.js';
 
 describe('Haiku', () => {
 
-  test('should correctly create a haiku object from a chunk of text and make array of lines', () => {
+  let haiku;
+
+  beforeEach(() => {
     const chunk = 
     `i like haikus
     i like you
     no i don't`;
-    const haiku = new Haiku(chunk);
+    haiku = new Haiku(chunk);
+  });
+
+  test('should correctly create a haiku object from a chunk of text and make array of lines', () => {
     expect(haiku.linesArray.length).toEqual(3);
+  });
+  test('should return true if array of lines has 3 elements', () => {
+    expect(haiku.checkLines()).toEqual(true);
   });
 });
