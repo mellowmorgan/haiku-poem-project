aecdf17685755f178387330eeff10bd7109c2596
mellowmorgan <morgan.waites@gmail.com>
2021-11-10 10:10:47 -0800

    Edit test for Haiku class

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,11 +2,12 @@ import { Haiku } from './../src/haiku.js';
 
 describe('Haiku', () => {
 
-  test('should correctly create a haiku object from a chunk of text and make array of lines', (chunk) => {
+  test('should correctly create a haiku object from a chunk of text and make array of lines', () => {
     const chunk = 
     `i like haikus
     i like you
     no i don't`;
+    const haiku = new Haiku(chunk);
     expect(this.linesArray.length).toEqual(3);
   });
 });
