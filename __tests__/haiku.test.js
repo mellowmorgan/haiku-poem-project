60437d86e589dfd5081818fa7ba0a3019607c37c
mellowmorgan <morgan.waites@gmail.com>
2021-11-10 13:41:39 -0800

    Test for checkSyllablesLines for multisyllable line passed

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -7,7 +7,7 @@ describe('Haiku', () => {
   beforeEach(() => {
     const chunk=`i
 i like you
-i am baby`;
+i am baby`; 
     haiku = new Haiku(chunk);
   });
 
@@ -35,8 +35,8 @@ i am baby`;
   test('should return "i am baby" when linesArray(2) is called', () => {
     expect(haiku.linesArray[2]).toEqual("i am baby");
   });
-  // test('should return 4 for a line with 4 syllables when checkSyllablesLine(line) called', () => {
-  //   expect(haiku.checkSyllablesLine(haiku.linesArray[2])).toEqual(4);
-  // });
+  test('should return 4 for a line with 4 syllables when checkSyllablesLine(line) called', () => {
+    expect(haiku.checkSyllablesLine(haiku.linesArray[2])).toEqual(4);
+  });
 
 });
