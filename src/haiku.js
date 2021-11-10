d5db1a7c11e433c2a5487478d8a899d16b75bc90
mellowmorgan <morgan.waites@gmail.com>
2021-11-10 10:26:48 -0800

    Pass test for method checkLines

--- src/haiku.js
+++ src/haiku.js
@@ -2,10 +2,10 @@ export class Haiku{
   constructor(chunk){
     this.linesArray=chunk.split(/[\n\r]+/);
   }
-  // checkLines(){
-  //   if (this.linesArray.length===3){
-  //     return true;
-  //   }
-  //   else{return false;}
-  // }
+  checkLines(){
+    if (this.linesArray.length===3){
+      return true;
+    }
+    else{return false;}
+  }
 }
